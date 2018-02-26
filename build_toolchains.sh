7dfe702622d3328f5e97d150cbf574bc3934538e
Rabenda <rabenda.cn@gmail.com>
2018-02-27 00:05:52 +0800

    Delete program prefix and add parameter about --with-gcc-major-version-only

--- build_toolchains.sh
+++ build_toolchains.sh
@@ -17,7 +17,7 @@ echo "check source is finish!"
 
 mkdir -p build
 cd build
-../gcc/configure --prefix=/opt/gcc-devel -v --with-pkgversion='gcc-devel' --enable-languages=c,c++  --program-prefix=x86_64-pc-linux-gnu- --enable-shared --enable-threads=posix --enable-nls --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-libmpx --enable-plugin --with-system-zlib --with-target-system-zlib --disable-werror --with-abi=m64 --disable-multilib --with-tune=generic --enable-offload-targets=nvptx-none --without-cuda-driver --enable-checking=release --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu
+../gcc/configure --prefix=/opt/gcc-devel -v --with-pkgversion='gcc-devel' --enable-languages=c,c++ --with-gcc-major-version-only --enable-shared --enable-threads=posix --enable-nls --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-libmpx --enable-plugin --with-system-zlib --with-target-system-zlib --disable-werror --with-abi=m64 --disable-multilib --with-tune=generic --enable-offload-targets=nvptx-none --without-cuda-driver --enable-checking=release --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu
 make -j 4
 sudo make install
 
